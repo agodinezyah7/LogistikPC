66010c88ba9bdf0d274cbf18193560e4d56d0122
agodinezyah7 <agodinez@bildden.com>
2025-01-08 14:03:33 -0600

    Update conexiones.vb

--- LogistikPC/conexiones.vb
+++ LogistikPC/conexiones.vb
@@ -3,5 +3,7 @@ Imports System.Data
 Imports System.Data.Sql
 Imports System.Data.SqlClient
 Module conexiones
-  
+      Public myConnectionString1 As String = "Server=CARLOS-LAPTOP\SQLEXPRESS;Database=UnitelasDB;Trusted_Connection=True; MultipleActiveResultSets=True"
+      Public myConnectionString2 As String = "Server=ROBERTO-PC\SQLEXPRESS;Database=UnitelasDB;Trusted_Connection=True; MultipleActiveResultSets=True"
+      Public myConnectionString3 As String = "Server=TECRA\SQLEXPRESS;Database=UnitelasDB;Trusted_Connection=True; MultipleActiveResultSets=True"
 End Module
